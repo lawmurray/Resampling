5716486a2bdf292fd2e38ef82e3e25803d08f1ac
Lawrence Murray <lawrence.murray@csiro.au>
2014-02-03 14:27:49 +0800

    Updated plot_times plot.

--- oct/plot_times.m
+++ oct/plot_times.m
@@ -13,50 +13,69 @@
 %
 function plot_times()
     clf;
-    backends = {
+    experiments = {
         'cpu';
+        'cpu-with-copy';
         'gpu';
+        'gpu-with-copy';
     };
     cs = {
-        'multinomial';
-        'stratified';
-	'systematic';
-        'metropolis';
-        'rejection';
-        'ess';
-        'sort';
-        };
+        'Multinomial';
+        'Stratified';
+	'Systematic';
+        'Metropolis';
+        'Rejection';
+        'ESS';
+        'Sort';
+    };
+    ax = [4 20 0 7];
     
-    for j = 1:length(backends)
-      subplot(1, length(backends), j);
+    for i = 1:length(experiments)
+      for j = 1:length(cs)
+          subplot(length(experiments), length(cs), (i - 1)*length(cs) + j);
+          
+          nc = netcdf(sprintf('results/%s-%s.nc', tolower(cs{j}), experiments{i}), 'r');
+          l2Ps = log2(nc{'P'}(:));
+          zs = nc{'z'}(:);
+          
+          for k = 1:2:length(zs)
+              times = log10(squeeze(nc{'time'}(k,:,:)));
+              
+              %middle = median(times, 2);
+              %upper = quantile(times, 0.975, 2);
+              %lower = quantile(times, 0.025, 2);
+              
+              %middle = mean(times, 2);
+              %upper = middle + std(times, 0, 2);
+              %lower = middle - std(times, 0, 2);
 
-      for i = 1:length(cs)
-        nc = netcdf(sprintf('results/%s-%s.nc', cs{i}, backends{j}), 'r');
-        P2 = log2(nc{'P'}(:));
-        times2 = squeeze(nc{'time'}(3,:,:));
-        %mean2 = mean(times2, 2);
-        %std2 = std(times2, 0, 2);
-        median2 = median(times2, 2);
-        upper2 = quantile(times2, 0.975, 2);
-        lower2 = quantile(times2, 0.025, 2);
-
-        %area_between(P2, max(mean2 - 2*std2, 1e-6), mean2 + 2*std2,
-        area_between(P2, lower2, upper2, watercolour(i), 1.0, 0.5);
-        hold on;
-        %h = semilogy(P2, mean2, linestyle(i), 'color', watercolour(i), 'linewidth', 5);
-        h = semilogy(P2, median2, linestyle(i), 'color', watercolour(i), 'linewidth', 5);
-        if j == 1
-          legend(h, sprintf('%c%s', upper(cs{i}(1)), cs{i}(2:end)));
-        end
-        grid on;
+              middle = mean(times, 2);
+              upper = middle;
+              lower = zeros(size(upper));
+              err = std(times, 0, 2);
+              
+              hold off;
+              area_between(l2Ps, lower, upper, watercolour(j), 1.0, 0.2);
+              hold on;
+              if strcmp(cs{j}, 'Rejection')
+                  h = errorbar(l2Ps, middle, err, '~');
+              else
+                  h = plot(l2Ps, middle);
+              end
+              set(h, 'linestyle', linestyle(j));
+              set(h, 'color', watercolour(j));
+              set(h, 'linewidth', zs(k));
+              if i == 1
+                  title(cs{j});
+              end
+              if i == length(experiments)
+                  xlabel('log_2 N');
+              end
+              if j == 1
+                  ylabel('Execution time (\mu s)');
+              end
+              grid on;
+              axis(ax);
       end
-
-      xlabel('log_2 N');
-      ylabel('\mu s');
-      if j == 1
-        legend("location", "NorthWest")
-        legend("right");
-      end
-      axis([min(P2) max(P2) 10 10^5]);
   end
 end
