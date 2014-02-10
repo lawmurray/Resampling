503b52409d71de7e5b2feb217b5aa423bcd1a03c
Lawrence Murray <lawrence.murray@csiro.au>
2014-02-10 16:37:44 +0800

    Updated test configurations.

--- run_metropolis.sh
+++ run_metropolis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CONFIG_OPTS="--nthreads 1 --with-cuda"
-COMMON_OPTS="--Zs 7 --Ps 15 --reps 256 --without-sort --seed 0 --disable-assert --enable-single --enable-cuda"
+CONFIG_OPTS="--nthreads 8 --with-cuda"
+COMMON_OPTS="--Zs 9 --Ps 17 --reps 256 --without-sort --seed 0 --disable-assert --enable-single --enable-cuda"
 
 libbi test_resampler $CONFIG_OPTS $COMMON_OPTS --resampler metropolis --output-file results/metropolis-c1.nc -C 1
 libbi test_resampler $CONFIG_OPTS $COMMON_OPTS --resampler metropolis --output-file results/metropolis-c2.nc -C 2
